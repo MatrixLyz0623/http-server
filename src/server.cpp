111fc51eca44539026f245e25d19f640421c0ea0
MatrixLyz <18500878538@163.com>
2025-07-26 17:19:04 +0800

    response

--- src/server.cpp
+++ src/server.cpp
@@ -53,7 +53,7 @@ int main(int argc, char **argv) {
   
   accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
-  
+  send(client_scoket,"HTTP/1.1 200 OK \r\n\r\n",20,0);
   close(server_fd);
 
   return 0;
